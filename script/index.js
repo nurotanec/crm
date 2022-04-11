f49b5a0f22467ad3702852dd9c7ad2aab23dec26
AidarS <nurotanec@bk.ru>
2022-04-11 20:05:52 +0600

    lesson_02 done

---
+++ script/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+{
+    const modalTitle = document.querySelector('.modal__title');
+    console.log('title: ', modalTitle);
+
+    const modalForm = document.querySelector('.modal__form');
+    console.log('form: ', modalForm);
+
+    const checkbox = document.querySelector('.modal__checkbox');
+    console.log('checkbox: ', checkbox);
+
+    const checkboxField = document.querySelector('.modal__input_discount');
+    console.log('checkboxField: ', checkboxField);
+}
