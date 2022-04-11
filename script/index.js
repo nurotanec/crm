ac668a8444b05b50c2cf58f3af80f4375fa91f32
AidarS <nurotanec@bk.ru>
2022-04-11 20:05:52 +0600

    lesson_02 done

---
+++ script/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+{
+    const title = document.querySelector('title');
+    console.log('title: ', title);
+
+    const form = document.querySelector('form');
+    console.log('form: ', form);
+
+    const checkbox = document.querySelector('.modal__checkbox');
+    console.log('checkbox: ', checkbox);
+
+    const checkboxField = document.querySelector('.modal__input_discount');
+    console.log('checkboxField: ', checkboxField);
+}
