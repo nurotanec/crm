27f8446593ea546bbc615b2cb24852b2ffcb160b
AidarS <nurotanec@bk.ru>
2022-04-16 22:27:45 +0600

    added modal open/close behaviour

--- script/index.js
+++ script/index.js
@@ -1,36 +1,37 @@
 'use strict';
 
-const createRow = (obj) => {
+{
+  const createRow = (obj) => {
     const tr = document.createElement('tr');
     tr.innerHTML = `<td class="table__cell">${obj.id}</td>
-    <td class="table__cell table__cell_left table__cell_name" data-id="24601654816512">
-      <span class="table__cell-id">id: 24601654816512</span>
-      ${obj.title}</td>
-    <td class="table__cell table__cell_left">${obj.category}</td>
-    <td class="table__cell">${obj.units}</td>
-    <td class="table__cell">${obj.count}</td>
-    <td class="table__cell">$${obj.price}</td>
-    <td class="table__cell">$${obj.price * obj.count}</td>
-    <td class="table__cell table__cell_btn-wrapper">
-      <button class="table__btn table__btn_pic"></button>
-      <button class="table__btn table__btn_edit"></button>
-      <button class="table__btn table__btn_del"></button>
-    </td>`
+        <td class="table__cell table__cell_left table__cell_name" data-id="24601654816512">
+        <span class="table__cell-id">id: 24601654816512</span>
+        ${obj.title}</td>
+        <td class="table__cell table__cell_left">${obj.category}</td>
+        <td class="table__cell">${obj.units}</td>
+        <td class="table__cell">${obj.count}</td>
+        <td class="table__cell">$${obj.price}</td>
+        <td class="table__cell">$${obj.price * obj.count}</td>
+        <td class="table__cell table__cell_btn-wrapper">
+        <button class="table__btn table__btn_pic"></button>
+        <button class="table__btn table__btn_edit"></button>
+        <button class="table__btn table__btn_del"></button>
+        </td>`
 
     return tr;
-}
+  }
 
-const renderGoods = (arr) => {
+  const renderGoods = (arr) => {
     const table = document.querySelector('.table__body')
     arr.forEach((obj) => {
-        const elem = createRow(obj);
-        table.append(elem);
+      const elem = createRow(obj);
+      table.append(elem);
     })
-}
+  }
 
-const goods =
-    [
-        {
+  const goods =
+        [
+          {
             "id": 1,
             "title": "Смартфон Xiaomi 11T 8/128GB",
             "price": 27000,
@@ -40,11 +41,11 @@ const goods =
             "count": 3,
             "units": "шт",
             "images": {
-                "small": "img/smrtxiaomi11t-m.jpg",
-                "big": "img/smrtxiaomi11t-b.jpg"
+              "small": "img/smrtxiaomi11t-m.jpg",
+              "big": "img/smrtxiaomi11t-b.jpg"
             }
-        },
-        {
+          },
+          {
             "id": 2,
             "title": "Радиоуправляемый автомобиль Cheetan",
             "price": 4000,
@@ -54,11 +55,11 @@ const goods =
             "count": 1,
             "units": "шт",
             "images": {
-                "small": "img/cheetancar-m.jpg",
-                "big": "img/cheetancar-b.jpg"
+              "small": "img/cheetancar-m.jpg",
+              "big": "img/cheetancar-b.jpg"
             }
-        },
-        {
+          },
+          {
             "id": 3,
             "title": "ТВ приставка MECOOL KI",
             "price": 12400,
@@ -68,11 +69,11 @@ const goods =
             "count": 4,
             "units": "шт",
             "images": {
-                "small": "img/tvboxmecool-m.jpg",
-                "big": "img/tvboxmecool-b.jpg"
+              "small": "img/tvboxmecool-m.jpg",
+              "big": "img/tvboxmecool-b.jpg"
             }
-        },
-        {
+          },
+          {
             "id": 4,
             "title": "Витая пара PROConnect 01-0043-3-25",
             "price": 22,
@@ -82,10 +83,41 @@ const goods =
             "count": 420,
             "units": "v",
             "images": {
-                "small": "img/lan_proconnect43-3-25.jpg",
-                "big": "img/lan_proconnect43-3-25-b.jpg"
+              "small": "img/lan_proconnect43-3-25.jpg",
+              "big": "img/lan_proconnect43-3-25-b.jpg"
             }
-        }
-    ];
+          }
+        ];
 
-renderGoods(goods);
+  const initButton = () => {
+
+  };
+
+  const init = () => {
+    renderGoods(goods);
+
+    // get all elements
+    const formOverlay = document.querySelector('.overlay');
+    const form = document.querySelector('.overlay__modal');
+    const addButton = document.querySelector('.panel__add-goods');
+    const closeButton = document.querySelector('.modal__close');
+
+    // add listeners
+    addButton.addEventListener('click', () => {
+      formOverlay.classList.add('active');
+    });
+    closeButton.addEventListener('click', () => {
+      formOverlay.classList.remove('active');
+    });
+
+    form.addEventListener('click', e => {
+      e.stopPropagation();
+    });
+
+    formOverlay.addEventListener('click', () => {
+      formOverlay.classList.remove('active');
+    });
+  }
+
+  window.crmInit = init;
+}
